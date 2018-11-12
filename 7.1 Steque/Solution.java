b5d21a9042181460be6d0aa4d1f54b5bb15cb6d1
deepikamsit <gundlapallideepika@msitprogram.net>
2018-11-12 15:29:46 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

---
+++ 7.1 Steque/Solution.java
@@ -0,0 +1,82 @@
+package deque;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+class stack {
+	LinkedList<Integer> ll;
+	public stack(){
+		ll = new LinkedList<Integer>();
+	}
+
+	public void push(int item) {
+		ll.addFirst(item);
+	}
+
+	public String pop() {
+		return (ll.isEmpty())?"Steque is empty.":(""+ll.removeFirst());
+		
+	}
+
+	public void enqueue(int item) {
+		ll.addLast(item);
+	}
+
+	public String print() {
+
+		if (ll.isEmpty()) {
+
+			return "Steque is empty.";
+		}
+		String st = "" + ll.get(0);
+		for (int i = 1; i < ll.size(); i++) {
+			st = st + ", " + ll.get(i);
+		}
+		return st;
+
+	}
+
+	public void removeAll() {
+		// TODO Auto-generated method stub
+		ll = new LinkedList<Integer>();
+	}
+}
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int n, data;
+		Scanner scan = new Scanner(System.in);
+		n = scan.nextInt();
+           stack st = new stack();
+		int flag = 0;
+		do {
+			String s = scan.nextLine();
+			String[] s1 = s.split(" ");
+			switch (s1[0]) {
+			case "enqueue":
+				data = Integer.parseInt(s1[1]);
+				st.enqueue(data);
+				System.out.println(st.print());
+				break;
+			case "push":
+				data = Integer.parseInt(s1[1]);
+				st.push(data);
+				System.out.println(st.print());
+				break;
+			case "pop":
+				st.pop();
+				System.out.println(st.print());
+				break;
+			default:
+				st.removeAll();
+				if(flag==1)
+				System.out.println();
+				flag=1;
+
+			}
+
+		} while (scan.hasNext());
+
+	}
+
+}
